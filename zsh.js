c4aa4924c81ef4a4b0f574cf9a7cf13e18a869aa
Charles Cui <alavin.cui@gmail.com>
2024-08-18 19:12:40 +0800

    Update zsh.js

--- zsh.js
+++ zsh.js
@@ -67,8 +67,8 @@ class UserInfo {
         try {
             let options = {
                 fn: "签到",
-                method: "post",
-                url: `https://activity-prd.saas.cmsk1979.com/api/marketing/campaign/v1/go`,
+                //method: "post",
+                //url: `https://activity-prd.saas.cmsk1979.com/api/marketing/campaign/v1/go`,
                 headers: {
                     "method": "POST",
                     "authority": "activity-prd.saas.cmsk1979.com",
