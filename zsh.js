37f4e1b76ff9559a4eff9d73a2eb162491e7ce21
Charles Cui <alavin.cui@gmail.com>
2024-08-19 21:39:13 +0800

    Update zsh.js

--- zsh.js
+++ zsh.js
@@ -70,7 +70,7 @@ class UserInfo {
             let options = {
                 fn: "用户信息",
                 method: "get",
-                url: `https://umopwx-api.saas.cmsk1979.com/be/api/activities/signIn`,
+                url: `https://umopwx-api.saas.cmsk1979.com/be/api/activities/signIn?cityCode=610100&loading=false`,
                 headers: {
                     "Host": "umopwx-api.saas.cmsk1979.com",
                     "charset": "utf-8",
