b2d6516b214fa1b420ced4b178475317db138202
Charles Cui <alavin.cui@gmail.com>
2024-08-20 00:25:29 +0800

    Update zsh.js

--- zsh.js
+++ zsh.js
@@ -105,7 +105,7 @@ class UserInfo {
             let options = {
                 fn: "签到",
                 method: "POST",
-                url: `https://activity-prd.saas.cmsk1979.com/api/marketing/campaign/v1/page-config?campaignId=2799365216249249792`,
+                url: `https://activity-prd.saas.cmsk1979.com/api/marketing/campaign/v1/go`,
                 headers: {
                     //"Host": "activity-prd.saas.cmsk1979.com",
                     //"cache-control": "no-cache",
@@ -125,9 +125,9 @@ class UserInfo {
                     //"sec-fetch-mode": "cors",
                     //"sec-fetch-dest": "empty",
                     //"accept-encoding": "gzip, deflate, br",
-		    "Referer": "https://activity-prd.saas.cmsk1979.com/mactivity/2694396930360655872/sign-in?authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiIxMTEwODc0NiIsInRva2VuIjoiNTUwMTcxNGIxNzdkNzI0YzFkNTNlNGY3MzExMzllY2VjOGQyYmFhZDRmNmY0NjYwMTc4MzA3YWI0YjZmMDZlZCIsIm1lbWJlcklkIjoiMTAwMDQ4NjEzMCJ9.YeRWtQ9RjB5Myn0WyARF_xFJTgQRWA7pfkZ6zcgta80&campaignId=2799365216249249792&capabilityId=2799365866676748288&cmsk_stamp=m00zw9a7&isSubscribe=false&token=5501714b177d724c1d53e4f731139ecec8d2baad4f6f4660178307ab4b6f06ed"
+		    //"Referer": "https://activity-prd.saas.cmsk1979.com/mactivity/2694396930360655872/sign-in?authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiIxMTEwODc0NiIsInRva2VuIjoiNTUwMTcxNGIxNzdkNzI0YzFkNTNlNGY3MzExMzllY2VjOGQyYmFhZDRmNmY0NjYwMTc4MzA3YWI0YjZmMDZlZCIsIm1lbWJlcklkIjoiMTAwMDQ4NjEzMCJ9.YeRWtQ9RjB5Myn0WyARF_xFJTgQRWA7pfkZ6zcgta80&campaignId=2799365216249249792&capabilityId=2799365866676748288&cmsk_stamp=m00zw9a7&isSubscribe=false&token=5501714b177d724c1d53e4f731139ecec8d2baad4f6f4660178307ab4b6f06ed"
                 },
-		//body: {"campaignId":"2799365216249249792","campaignType":"INTERACTIVE","capabilityId":"2799365866676748288","capabilityType":"SIGN_IN"},
+		body: JSON.stringify({"campaignId":"2799365216249249792","campaignType":"INTERACTIVE","capabilityId":"2799365866676748288","capabilityType":"SIGN_IN"})
             }
             let result  = await httpRequest(options);
             console.log(options);
