5b2947842f844108177f873c7f3afa495072b30d
Charles Cui <alavin.cui@gmail.com>
2024-08-19 22:47:20 +0800

    Update zsh.js

--- zsh.js
+++ zsh.js
@@ -107,19 +107,19 @@ class UserInfo {
                 method: "post",
                 url: `https://activity-prd.saas.cmsk1979.com/api/marketing/campaign/v1/go`,
                 headers: {
-                    "Host": "activity-prd.saas.cmsk1979.com",
+                    //"Host": "activity-prd.saas.cmsk1979.com",
                     //"cache-control": "no-cache",
                     //"pragma": "no-cache",
                     //"accept-language": "zh-CN",
                     //"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 MicroMessenger/6.8.0(0x16080000) NetType/WIFI MiniProgramEnv/Mac MacWechat/WMPF MacWechat/3.8.7(0x13080710) XWEB/1191",
                     //"content-type": "application/json; charset=UTF-8",
-                    "accept": "application/json",
+                    //"accept": "application/json",
                     //"x-requested-with": "XMLHttpRequest",
-                    "authorization": this.ck,
-                    "capabilityid": "2799365866676748288",
-                    "campaignid": "2799365216249249792",
-                    "capabilitytype": "SIGN_IN",
-                    "campaignType": "INTERACTIVE"
+                    //"authorization": this.ck,
+                    "campaignId": "2799365216249249792",
+	                "campaignType": "INTERACTIVE",
+	                "capabilityId": "2799365866676748288",
+	                "capabilityType": "SIGN_IN"
                     //"origin": "https://activity-prd.saas.cmsk1979.com"
                     //"sec-fetch-site": "same-origin",
                     //"sec-fetch-mode": "cors",
