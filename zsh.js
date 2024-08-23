a5434586b65c6f2c20def6efbc12819aea903b91
Charles Cui <alavin.cui@gmail.com>
2024-08-23 18:23:52 +0800

    Update zsh.js

--- zsh.js
+++ zsh.js
@@ -125,7 +125,7 @@ class UserInfo {
 		    "accept-encoding": "gzip,deflate,br"
                 },
 		//body: JSON.stringify({})
-		body: JSON.stringify({
+		body: JSON.parse(({
 		    "campaignid": "2799365216249249792"
 		})
             }
