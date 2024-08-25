896e555ac3f18b6c7ee585757dc569af61d4707a
Charles Cui <alavin.cui@gmail.com>
2024-08-25 10:50:24 +0800

    Update EMS.py

--- EMS.py
+++ EMS.py
@@ -3,7 +3,7 @@
 # -------------------------------
 # ✨✨✨ @Author CHERWIN✨✨✨
 # -------------------------------
-# cron "10 0 * * *" script-path=xxx.py,tag=匹配cron用
+# cron "21 0 * * *" script-path=xxx.py,tag=匹配cron用
 # const $ = new Env('EMS邮惠中心')
 
 import os
