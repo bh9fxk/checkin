43dcd5d829565b151883a84f300601e1de342f19
Charles Cui <alavin.cui@gmail.com>
2024-08-25 22:51:37 +0800

    Update zsh.js

--- zsh.js
+++ zsh.js
@@ -40,7 +40,7 @@ class UserInfo {
                     "Host": "umopwx-api.saas.cmsk1979.com",
 		    "Connection": "Keep-Alive",
                     "charset": "utf-8",
-                    "authorization": this.ck[0],
+                    "authorization": this.ck[1],
                     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 MicroMessenger/6.8.0(0x16080000) NetType/WIFI MiniProgramEnv/Mac MacWechat/WMPF MacWechat/3.8.7(0x13080710) XWEB/1191",
                     "Content-Type": "application/json",
                     "Accept-Encoding": "gzip,deflate,br",
