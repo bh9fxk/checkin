7d5afef574f1a73963010eb65fd6503d663b4035
Charles Cui <alavin.cui@gmail.com>
2024-08-26 13:57:27 +0800

    Update zsh.js

--- zsh.js
+++ zsh.js
@@ -32,6 +32,14 @@ class UserInfo {
     }
     async user_point() {
         try {
+	    const https = require('https')
+	    const options = {
+	    hostname: 'nodejs.cn',
+	    port: 443,
+	    path: '/todos',
+	    method: 'GET'
+	    }
+	
             let options = {
                 fn: "积分查询",
                 method: "get",
@@ -39,7 +47,7 @@ class UserInfo {
                 headers: {
                     "Host": "umopwx-api.saas.cmsk1979.com",
 		    "Connection": "Keep-Alive",
-                    "charset": "utf-8",
+                    //"charset": "utf-8",
                     "authorization": this.ck,
                     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 MicroMessenger/6.8.0(0x16080000) NetType/WIFI MiniProgramEnv/Mac MacWechat/WMPF MacWechat/3.8.7(0x13080710) XWEB/1191",
                     "Content-Type": "application/json",
@@ -49,7 +57,7 @@ class UserInfo {
             }
             let result  = await httpRequest(options);
             //console.log(options);
-            result = JSON.parse(result);
+            //result = JSON.parse(result);
             //console.log(result);
             if (result["code"] == null) {
                 console.log(`✅${options.fn}成功 当前积分[${result}]🎉`);
