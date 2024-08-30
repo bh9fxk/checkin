e0aaea14279b43a15447f9d085302d1bda725339
Charles Cui <alavin.cui@gmail.com>
2024-08-30 14:42:58 +0800

    Update xsc.js

--- xsc.js
+++ xsc.js
@@ -93,7 +93,12 @@ class UserInfo {
     async signIn() {
         try {
 	    const https = require('https')
-	    const data = {module=app&action=sign&app=index&access_id=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MjQ5MzU5MDgsImV4cCI6MTcyNDk0MzEwOCwianRpIjoiOWQxY2YyMmNjYTk5MjdjYzI2MDRhOTIyZGU3NjRjZjAifQ.h_VR0YJXHVi0lOiIst6jSHdaj2METJEhujmS1WhxR5Q}
+	    const form = {
+		    'module': 'app',
+		    'action': 'sign',
+		    'app': 'index',
+		    'access_id': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MjQ5MzU5MDgsImV4cCI6MTcyNDk0MzEwOCwianRpIjoiOWQxY2YyMmNjYTk5MjdjYzI2MDRhOTIyZGU3NjRjZjAifQ.h_VR0YJXHVi0lOiIst6jSHdaj2METJEhujmS1WhxR5Q'
+	    }
 
 	    const options = {
 		hostname: 'mall.bicrm.cn',
