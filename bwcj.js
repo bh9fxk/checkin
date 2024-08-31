396328cba5bcb190fb99e1d99cf6767ae51c86d5
Charles Cui <alavin.cui@gmail.com>
2024-08-31 17:38:56 +0800

    Update bwcj.js

--- bwcj.js
+++ bwcj.js
@@ -94,10 +94,11 @@ class UserInfo {
         try {
 	    const https = require('https')
 	    console.log(Date.now())
+	    let this.ts = Date.now()
 	    const data = JSON.stringify({
 		"activityId": "947079313798000641",
 		"storeId": 49006,
-		"timestamp": Date.now(),
+		"timestamp": this.ts,
 		"signature": "89F1442E5325824B9779C70195F7EB76",
 		"appid": "wxafec6f8422cb357b",
 		"store_id": 49006
