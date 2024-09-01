fd3f5981781316d2197d628b8c770f72733c2fa4
Charles Cui <alavin.cui@gmail.com>
2024-09-01 13:37:14 +0800

    Update mmpk.js

--- mmpk.js
+++ mmpk.js
@@ -52,7 +52,7 @@ class UserInfo {
 		if (`${res.statusCode}` == 200) {
                     res.on('data', d => {
                         //process.stdout.write(d)
-                        let result = JSON.parse(d)
+                        let result = JSON.parse(JSON.stringify(d）)
 		        console.log(result)
 		        console.log(`\n登录状态：【${result.msg}】`)
 			console.log(`\n现总积分：【${result.data.integral}】`)
