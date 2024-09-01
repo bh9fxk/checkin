84e3d52150498d78615109c935e510a6a9974d95
Charles Cui <alavin.cui@gmail.com>
2024-09-01 13:24:03 +0800

    Update bwcj.js

--- bwcj.js
+++ bwcj.js
@@ -97,7 +97,7 @@ class UserInfo {
 	    const data = JSON.stringify({
 		"activityId": "947079313798000641",
 		"storeId": 49006,
-		"timestamp": Date.now(),
+		"timestamp": "1725074610429",
 		"signature": "89F1442E5325824B9779C70195F7EB76",
 		"appid": "wxafec6f8422cb357b",
 		"store_id": 49006
