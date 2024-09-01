2f4d846318c62a057c9f64ac563697b69a746fa5
Charles Cui <alavin.cui@gmail.com>
2024-09-01 14:14:17 +0800

    Update mmpk.js

--- mmpk.js
+++ mmpk.js
@@ -104,7 +104,7 @@ class UserInfo {
 			let aa = String(d)
 			console.log(d)
                         let result = JSON.stringify(d)
-		        /console.log(result)
+		        //console.log(result)
 			//console.log(JSON.parse(result.data))
 		        //console.log(`\n签到结果：【${result.msg}】`);
 		        //msg += `\n签到结果：【${result.msg}】`
