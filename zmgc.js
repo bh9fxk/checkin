5954b46dc84e7cd3b616d3109ba41723d30838a5
Charles Cui <alavin.cui@gmail.com>
2024-09-08 15:03:19 +0800

    Update zmgc.js

--- zmgc.js
+++ zmgc.js
@@ -7,7 +7,7 @@
  * scriptVersionNow = "0.0.1";
  */
 
-const $ = new Env("金辉广场签到");
+const $ = new Env("中贸广场签到");
 const notify = $.isNode() ? require('./sendNotify') : '';
 const Notify = 1; // 1开启通知
 let ckName = "zmgc_ck";
