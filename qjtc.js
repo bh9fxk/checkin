bff80a2879668165497779504f54ac22b8657bb3
Charles Cui <alavin.cui@gmail.com>
2024-09-19 17:34:48 +0800

    Update qjtc.js

--- qjtc.js
+++ qjtc.js
@@ -176,7 +176,7 @@ class UserInfo {
 		"days": days,
 		"createTime": null,
 		"signToday": true,
-		"signConfig": '"{\"signType\":2,\"signDays\":\"7\",\"cumType\":1,\"name\":\"用户签到\"}"',
+		"signConfig": {\"signType\":2,\"signDays\":\"7\",\"cumType\":1,\"name\":\"用户签到\"},
 		"openFlag": true
             })
 	    //console.log(data)
