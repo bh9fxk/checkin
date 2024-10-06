18424e5c52b3f06ceb2a215ac4e282d96c6c7d67
Charles Cui <alavin.cui@gmail.com>
2024-10-06 23:09:11 +0800

    Update lb.js

--- lb.js
+++ lb.js
@@ -31,8 +31,8 @@ class UserInfo {
 	await this.coupon()
 	await $.wait(3000)
 	await this.signIn()
-	//await $.wait(3000)
-	//await SendMsg(msg)
+	await $.wait(3000)
+	await SendMsg(msg)
     }
 
     async point() {
