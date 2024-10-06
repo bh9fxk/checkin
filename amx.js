2c89f17a7f92d546135726a43caed6edda545120
Charles Cui <alavin.cui@gmail.com>
2024-10-06 22:35:19 +0800

    Update amx.js

--- amx.js
+++ amx.js
@@ -28,8 +28,8 @@ class UserInfo {
         await this.point()
 	await $.wait(3000)
 	await this.signIn()
-	//await $.wait(3000)
-	//await SendMsg(msg)
+	await $.wait(3000)
+	await SendMsg(msg)
     }
 
     async point() {
