8e3de99255418aaff8b1210ff95396b2a156ebb0
Charles Cui <alavin.cui@gmail.com>
2024-10-07 18:22:33 +0800

    Update ylnn.js

--- ylnn.js
+++ ylnn.js
@@ -31,8 +31,8 @@ class UserInfo {
 	await this.point()
 	await $.wait(3000)
         await this.signIn()
-	//await $.wait(3000)
-	//await SendMsg(msg)
+	await $.wait(3000)
+	await SendMsg(msg)
     }
 
     async coupon() {
