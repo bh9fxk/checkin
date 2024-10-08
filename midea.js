d2cf02de43dac595d5f8583653eeb1eb67eed0fd
Charles Cui <alavin.cui@gmail.com>
2024-10-08 19:18:08 +0800

    Update midea.js

--- midea.js
+++ midea.js
@@ -31,8 +31,8 @@ class UserInfo {
 	await this.user()
 	await $.wait(3000)
         await this.signIn()
-	//await $.wait(3000)
-	//await SendMsg(msg)
+	await $.wait(3000)
+	await SendMsg(msg)
     }
 
     async user() {
