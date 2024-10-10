192781598620aaaa51294f46b6120dd64aa904e3
Charles Cui <alavin.cui@gmail.com>
2024-10-10 13:46:49 +0800

    Update NXDD.py

--- NXDD.py
+++ NXDD.py
@@ -3,7 +3,7 @@
 # -------------------------------
 # @Author CHERWIN✨✨✨
 # -------------------------------
-# cron "1 0,8 * * *" script-path=xxx.py,tag=匹配cron用
+# cron "0 0,8 * * *" script-path=xxx.py,tag=匹配cron用
 # const $ = new Env('奈雪小程序')
 import datetime
 import json
