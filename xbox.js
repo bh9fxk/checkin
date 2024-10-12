9ed5a9f0fb15833bd027919ab0d0577bd13a0820
Charles Cui <alavin.cui@gmail.com>
2024-10-12 23:04:50 +0800

    Update xbox.js

--- xbox.js
+++ xbox.js
@@ -109,7 +109,7 @@ class UserInfo {
 		    //'Content-Length': data.length,
 		    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 MicroMessenger/6.8.0(0x16080000) NetType/WIFI MiniProgramEnv/Mac MacWechat/WMPF MacWechat/3.8.8(0x13080810) XWEB/1227',
 		    //'Accept-Encoding': 'gzip,compress,br,deflate', 
-		    'extra-data': '{"is_weapp":1,"sid":'+this.sid+',"uuid":'+this.uuid+',"ftime":1728719483865}'
+		    'extra-data': '{"is_weapp":1,"sid":'+this.sid+',"uuid":'+this.uuid+',"version":"2.149.9.101","client":"weapp","bizEnv":"wsc","ftime":1728719483865}'
 		}
 	    }
 	    const req = https.request(options, res => {
