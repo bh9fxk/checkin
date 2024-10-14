eed5a71298e55971924293164e9c974930581bcc
Charles Cui <alavin.cui@gmail.com>
2024-10-14 20:25:59 +0800

    Update xbox.js

--- xbox.js
+++ xbox.js
@@ -102,9 +102,10 @@ class UserInfo {
 	    //转换一次后，数据运行正常
 	    let json = JSON.stringify({"is_weapp":1,"sid":this.sid,"uuid":this.uuid})
 	    let json1 = json.toString()
+	    let json2 = JSON.parse(json1)
 	    console.log("--------------")
 	    console.log(json1)
-	    console.log(json)
+	    console.log(json2)
 	    const options = {
 		hostname: 'h5.youzan.com',
 		port: 443,
