81a2279a270dec15e524c52867e87845672695dd
Charles Cui <alavin.cui@gmail.com>
2024-10-14 20:31:06 +0800

    Update xbox.js

--- xbox.js
+++ xbox.js
@@ -101,7 +101,7 @@ class UserInfo {
 	    //const data = JSON.stringify({})
 	    //转换一次后，数据运行正常
 	    let json = JSON.stringify({"is_weapp": 1,"sid": this.sid,"uuid": this.uuid})
-	    let json1 = console.log({"is_weapp": 1,"sid": this.sid,"uuid": this.uuid})
+	    let json1 = console.log({"is_weapp":1,"sid":this.sid,"uuid":this.uuid})
 	    let json2 = JSON.parse(json1)
 	    console.log("--------------")
 	    console.log(json1)
