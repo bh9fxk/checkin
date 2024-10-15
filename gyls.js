80a8d0a7ef909ecd8c3053ca8b574abcda0dd846
Charles Cui <alavin.cui@gmail.com>
2024-10-15 10:47:55 +0800

    Update gyls.js

--- gyls.js
+++ gyls.js
@@ -128,10 +128,12 @@ class UserInfo {
 			console.log(result)
 			if (result.code == 0) {
 			    console.log(`\n签到信息：【${result.msg}】`)
-			    console.log(`\n今日签到：【${result.data.list[0].infos.title}】`)
+			    //console.log(`\n今日签到：【${result.data.list[0].infos.title}】`)
+			    console.log(`\n今日签到：【${result.data.desc}】`)
 			    console.log(`\n已经签到：【${result.data.times}】天`)
 			    msg += `\n签到信息：【${result.msg}】`
-			    msg += `\n今日签到：【${result.data.list[0].infos.title}】`
+			    //msg += `\n今日签到：【${result.data.list[0].infos.title}】`
+			    msg += `\n今日签到：【${result.data.desc}】`
 			    msg += `\n已经签到：【${result.data.times}】天`
 			} else {
 			    console.log(`\n签到信息：【${result.msg}】`)
